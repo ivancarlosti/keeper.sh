0e4ea4fbff3d754dc97590113ccf9ba09af6dc2d
ridafkih <hello@rida.dev>
2025-12-18 19:49:13 -0500

    fixup! feat(cron): add cron job to snapshot all remote icap

--- packages/cron/src/index.ts
+++ packages/cron/src/index.ts
@@ -4,4 +4,4 @@ import { join } from "node:path";
 
 const jobsFolderPathname = join(import.meta.dirname, "jobs");
 
-const jobs = await getAllJobs(jobsFolderPathname).then(registerJobs);
+getAllJobs(jobsFolderPathname).then(registerJobs);
