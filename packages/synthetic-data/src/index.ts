e36d36752f226435844d3acc8a80328b68b3963d
ridafkih <hello@rida.dev>
2025-12-24 11:31:09 -0500

    fixup! feat: use turbo, add types command

--- packages/synthetic-data/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./generators/users";
-export * from "./generators/calendars";
-export * from "./generators/remote-sources";
-export * from "./generators/snapshots";
-export * from "./generators/event-states";
-
-export * from "./utils/random";
